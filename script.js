7de5cd5e28907a334ea1048c759f4ee644075d2d
ahwatts <ahwatts@umich.edu>
2022-05-12 23:45:53 -0700

    jjddj

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log('helloworld');
+
+$('#hi').on({
+    'click': function(){
+        $("#change-image").fadeOut("slow", function() {
+            $('#change-image').css('background-image', "url('2k.png')");
+            $("#change-image").fadeIn("slow");
+        });             
+    }
+});
