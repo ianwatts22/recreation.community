24bfbbc4625aad5cb8de46b285de5f6b6a454e6d
ahwatts <ahwatts@umich.edu>
2022-05-16 15:58:56 -0700

    video

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ var storage = "url('images/caps.png')";
 var grinding = "url('images/grinding.png')";
 var container = "url('images/container.png')";
 
+/*function loadImages() {
+    magnet = "url('images/caps.png')";
+    ring = "url('images/assembled.png')";   
+    grinding = "url('images/grinding.png')";
+    container = "url('images/container.png')";
+    storage = "url('images/caps.png')";
+}*/
 
 function changeImage(image){
     $("#change-image").fadeOut("slow", function() {
