280ab867a75bd8d6ca33a2e1663655f0a8e03478
ahwatts <ahwatts@umich.edu>
2022-05-16 00:34:19 -0700

    j

--- script.js
+++ script.js
@@ -1,45 +1,14 @@
+var magnet = "url('images/caps.png')";
+var ring = "url('images/assembled.png')";
+var storage = "url('images/caps.png')";
+var grinding = "url('images/grinding.png')";
+var container = "url('images/container.png')";
 
-$('#magnet-btn').on({
-    'click': function(){
-        $("#change-image").fadeOut("slow", function() {
-            $('#change-image').css('background-image', "url('images/caps.png')");
-            $("#change-image").fadeIn("slow");
-        });             
-    }
-});
 
-$('#o-ring-btn').on({
-    'click': function(){
-        $("#change-image").fadeOut("slow", function() {
-            $('#change-image').css('background-image', "url('images/assembled.png')");
-            $("#change-image").fadeIn("slow");
-        });             
-    }
-});
-
-$('#storage-btn').on({
-    'click': function(){
-        $("#change-image").fadeOut("slow", function() {
-            $('#change-image').css('background-image', "url('images/storage.png')");
-            $("#change-image").fadeIn("slow");
-        });             
-    }
-});
-
-$('#grinding-btn').on({
-    'click': function(){
-        $("#change-image").fadeOut("slow", function() {
-            $('#change-image').css('background-image', "url('images/grinding.png')");
-            $("#change-image").fadeIn("slow");
-        });             
-    }
-});
-
-$('#container-btn').on({
-    'click': function(){
-        $("#change-image").fadeOut("slow", function() {
-            $('#change-image').css('background-image', "url('images/container.png')");
-            $("#change-image").fadeIn("slow");
-        });             
-    }
-});
+function changeImage(image){
+    $("#change-image").fadeOut("slow", function() {
+        $('#change-image').css('background-image', image);
+        $("#change-image").fadeIn("slow");
+        console.log(image);
+    });
+}
