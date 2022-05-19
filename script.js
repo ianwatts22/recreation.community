7e79620f7b496825ae8d1943a8bde01a3779bead
ahwatts <ahwatts@umich.edu>
2022-05-19 12:39:21 -0700

    duwop

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var magnet = "url('images/caps.png')";
-var ring = "url('images/assembled.png')";
-var storage = "url('images/caps.png')";
+var magnet = "url('images/magnet.png')";
+var oring = "url('images/oring.png')";
+var storage = "url('images/storage.png')";
 var grinding = "url('images/grinding.png')";
 var container = "url('images/container.png')";
 
@@ -12,6 +12,27 @@ var container = "url('images/container.png')";
     storage = "url('images/caps.png')";
 }*/
 
+var vid = document.getElementById("myVideo");
+
+function myFunction() { 
+  $("#myVideo").attr("src", "videos/magnet.mp4");
+  console.log("jaqn");
+} 
+
+function myFunction2() { 
+  $("#myVideo").attr("src", "videos/teeth.mp4");
+  console.log("ff");
+}
+
+function changeVideo(){
+    $("#change-image").fadeOut("slow", function() {
+        //$('#video').setAttribute('src', "url('teeth.mp4')");
+        $('#video-source').src = 'magnet.mp4';
+        $("#change-image").fadeIn("slow");
+        console.log("ciao");
+    });
+}
+
 function changeImage(image){
     $("#change-image").fadeOut("slow", function() {
         $('#change-image').css('background-image', image);
@@ -19,3 +40,11 @@ function changeImage(image){
         console.log(image);
     });
 }
+
+
+window.addEventListener('resize', function(event){
+  console.log("hi");
+  if($(".myVideo")[0].paused) {
+    $(".myVideo")[0].play();
+  }
+});
