a297cb7455a0e288ec1d3c0f30d8769d9dc26b04
ahwatts <ahwatts@umich.edu>
2022-06-02 17:02:44 -0700

    djd
    Former-commit-id: 0f3ae41f722b86cbe2ee5eacabe3c5a6dc956041
    Former-commit-id: d63a9ea81aaf16bef851e60ff7fb2277982b6af5

--- script.js
+++ script.js
@@ -1,35 +1,9 @@
-var magnet = "url('images/magnet.png')";
-var oring = "url('images/oring.png')";
-var storage = "url('images/storage.png')";
-var grinding = "url('images/grinding.png')";
-var container = "url('images/container.png')";
-
-/*function loadImages() {
-    magnet = "url('images/caps.png')";
-    ring = "url('images/assembled.png')";   
-    grinding = "url('images/grinding.png')";
-    container = "url('images/container.png')";
-    storage = "url('images/caps.png')";
-}*/
-
-
-var vid = document.getElementById("myVideo");
-
-function myFunction() { 
-  $("#myVideo").attr("src", "videos/magnet.mp4");
-  console.log("jaqn");
-} 
-
-function myFunction2() { 
-  $("#myVideo").attr("src", "videos/teeth.mp4");
-  console.log("ff");
-}
 
 function changeVideo(){
     $("#change-image").fadeOut("slow", function() {
-        //$('#video').setAttribute('src', "url('teeth.mp4')");
+        $('#video').setAttribute('src', "url('teeth.mp4')");
         $("#change-image").fadeIn("slow");
-        console.log("ciao");
+        console.log("change video");
     });
 }
 
@@ -37,21 +11,23 @@ function changeImage(image){
     $("#change-image").fadeOut("slow", function() {
         $('#change-image').css('background-image', image);
         $("#change-image").fadeIn("slow");
-        console.log(image);
+        console.log("change image");
     });
 }
 
-
-window.addEventListener('resize', function(event){
+//continue video on screen resize
+/*window.addEventListener('resize', function(event){
   console.log("hi");
   if($(".myVideo")[0].paused) {
     $(".myVideo")[0].play();
   }
-});
+});*/
 
-$('#myVideo').bind('ended', function(){
-  //$('#myVideo').fadeOut()
-  console.log("ciao");
+
+//fade after video
+$('#random').bind('ended', function(){
+  $('#random').fadeOut()
+  console.log("fade after video");
 });
 
 
@@ -60,11 +36,15 @@ function hide(){
 }
 
 function vid1() {
+  $("#real-video")[0].pause();
   $("#real-video").attr("src", "videos/vid1.mp4");
+  $("#real-video")[0].load();
   $('#real-video')[0].play();
 }
 
 function vid2() {
+  $("#real-video").pause();
   $("#real-video").attr("src", "videos/vid2.mp4");
+  $("#real-video").load();
   $('#real-video')[0].play();
 }
