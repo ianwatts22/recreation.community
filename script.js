d85bbdd2b324b334a5c033d6fb115ee93a9b736b
ahwatts <ahwatts@umich.edu>
2022-06-07 16:01:14 -0700

    hekllo
    Former-commit-id: 441d85be391375344ed6986b1b2ee50c88889c05

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function checkPswd() {
   var confirmPassword = "turtle";
   var password = document.getElementById("pswd").value;
   if (password == confirmPassword) {
-      window.location="proto.html";
+      window.location="grinder.html";
   }
   else{
       alert("Passwords do not match.");
@@ -10,14 +10,48 @@ function checkPswd() {
 }
 
 var video1 = "videos/green1.mp4";
-var video2 = "videos/green1.mp4";
-var video3 = "videos/green1.mp4";
+var video2 = "videos/green2.mp4";
+var video3 = "videos/green3.mp4";
+const videos = [video1, video2, video3];
+var videonum = 1;
 
+var rad1 = document.getElementById("rad1");
+var rad2 = document.getElementById("rad2");
+var rad3 = document.getElementById("rad3");
+const rads = [rad1, rad2, rad3];
+
+var label1 = document.getElementById("label1");
+var label2 = document.getElementById("label2");
+var label3 = document.getElementById("label3");
+const labels = [label1, label2, label3];
 
 function vid(video) {
   $("#video")[0].pause();
-  console.log(video);
   $("#video").attr("src", video);
   $("#video")[0].load();
   $('#video')[0].play();
+}
+
+$('#video').bind('ended', function(){
+  console.log(videos[videonum]);
+  vid(videos[videonum]);
+  rads[videonum].checked = true;
+  label1.style.background = "hsla(0, 0%, 80%, .0)";
+  label2.style.background = "hsla(0, 0%, 80%, .0)";
+  label3.style.background = "hsla(0, 0%, 80%, .0)";
+  labels[videonum].style.background = "hsla(0, 0%, 80%, .15)";
+  videonum++;
+  if(videonum > 2){
+    videonum = 0;
+  }
+});
+
+function toggleVid() {
+  if($("#video")[0].paused){
+    $('#video')[0].play();
+  }
+  else
+  {
+    $('#video')[0].pause();
+  }
 }
