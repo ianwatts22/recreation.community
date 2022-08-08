c47bfc35ed490ba6be4e07cd03be66fad24427e5
ianwatts22 <ian@ianwatts.site>
2022-08-08 12:00:10 -0700

    split videos, started on smooth vid transition
    Former-commit-id: 39f4160f168d6743d4c16b6f2c7e7f278df538b5
    Former-commit-id: d7529a72e1eb93cd6ce1104f8b55305922f7862c
    Former-commit-id: 9ac14005d415ed7c10f9ae2fb36514f4181c33ba [formerly 2c8015872018c3479a6dabf02b3c62280afd8ed1]
    Former-commit-id: 17f464ffbb143873998022b199234b3514b2b85e
    Former-commit-id: 66cb9545fad1698db54e44bffcc7bd7ce1ce0559

--- script.js
+++ script.js
@@ -1,40 +1,58 @@
 // VIDEO
-/* var video1 = "videos/website intro 2.mp4";
-var video2 = "videos/website intro 2.mp4";
-var video3 = "videos/website intro 2.mp4";
-const videos = [video1, video2, video3];
-var videonum = 1; */
 
+var vidElement = document.getElementById('video');  //HTML video element
+var nextVidElement = document.createelement
+const video1 = "videos/grinder demo.mp4";
+const video2 = "videos/grinder vibes.mp4";
+const videos = [video1, video2];
+var activeVideo = 0;
 
-
-const video = "videos/website intro 3.mp4";
 
 // a video is an array (of frames?) so to reference 
 function vid(video) {
-  $("#video")[0].pause();
+  /* $("#video")[0].pause();
   $("#video").attr("src", video);
   $("#video")[0].load();
-  $('#video')[0].play();
+  $('#video')[0].play(); */
+
+  vidElement.pause();
+  vidElement.src = video;
+  vidElement.load();
+  vidElement.play();
+
 }
 
-/* 
-$('#video').bind('ended', function(){
-  console.log(videos[videonum]);
-  vid(videos[videonum]);
-  // rads[videonum].checked = true;
-
-  videonum++;
-  if(videonum > 2){
-    videonum = 0;
+vidElement.addEventListener('ended', function(e) {
+  // update the active video index
+  activeVideo += 1;
+  if(activeVideo == videos.length){
+    activeVideo = 0;
   }
+
+  vid(videos[activeVideo]);
 });
- */
+
+
+/* $('#video').bind('ended', function() {
+  vid(videos[activeVideo]);          // playing video
+  
+  activeVideo++;
+  if(activeVideo > videos.length - 1) {
+    activeVideo = 0;
+  }
+}); */
 
 function toggleVid() {
-  if($("#video")[0].paused){
+  /* if($("#video")[0].paused){
     $('#video')[0].play();
   } else {
     $('#video')[0].pause();
+  } */
+
+  if(vidElement.paused){
+    vidElement.play();
+  } else {
+    vidElement.pause();
   }
 }
 
@@ -73,4 +91,52 @@ function showQR() {
   qrCode.toggle("hidden");
   blurBackground();
   clickawayBG();
+}
+
+
+
+
+
+// (source: https://stackoverflow.com/questions/52514522/html5-video-how-to-seamlessly-play-several-videos-and-then-loop-the-sequence )
+var videoContainer = document.getElementById('main1');
+nextVideo;
+videoObjects = [document.createElement('video'), document.createElement('video')];
+nextActiveVideo = 0;
+
+videoObjects[0].inx = 0; //set index
+videoObjects[1].inx = 1;
+
+initVideoElement(videoObjects[0]);
+initVideoElement(videoObjects[1]);
+
+videoObjects[0].src = videos[nextActiveVideo];
+videoContainer.appendChild(videoObjects[0]);
+
+videoObjects[1].style.display = 'none';
+videoContainer.appendChild(videoObjects[1]);
+
+function initVideoElement(video) {
+    video.playsinline = true;
+    video.muted = false;
+    video.preload = 'auto'; //WARNING: autoplay deletes preload, loadedmetadata gives endless loop
+
+    video.onplaying = function(e) {
+        //select next index. If is equal videos.length then it is 0
+        nextActiveVideo = ++nextActiveVideo % videos.length;
+
+        //replace the video elements against each other:
+        if(this.inx == 0) {
+            nextVideo = videoObjects[1]; }
+        else {
+            nextVideo = videoObjects[0]; }
+
+        nextVideo.src = videos[nextActiveVideo];
+        nextVideo.pause();
+    };
+
+    video.onended = function(e) {
+        this.style.display = 'none';
+        nextVideo.style.display = 'block';
+        nextVideo.play();
+    };
 }
